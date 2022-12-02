09fd710cfb5bd8f1745bffd1b3ef8f6b1133bf50
Shawn Squire <shawn@zeroedin.com>
2022-12-02 00:26:46 -0500

    Added Phase 2; which includes re-arranging numbers

--- src/code.py
+++ src/code.py
@@ -2,7 +2,13 @@
 
 # Pull the product function from itertools to make permutation generation with repeats
 # We will use the chain to combine with zipping for later
-from itertools import product, chain
+# Permutations comes later when we want to re-arrange the numbers
+from itertools import product, chain, permutations
+
+# Mode to play with.
+# Phase 1 -> Don't rearrange
+# Phase 2 -> Check rearrangements
+PHASE = 2
 
 # Target value to hit
 TARGET = 10
@@ -77,17 +83,66 @@ def eval_formula(challenge, op):
 
 # Iterate through each possible challenge and find all the operator combinations that equal the TARGET
 solutions = {challenge: [ op for op in ops if eval_formula(challenge, op) == TARGET ] for challenge in challenges}
-print(sum([1 if len(x) == 0 else 0 for x in solutions]), "Challenge without solutions")
+print(sum([1 if len(x) == 0 else 0 for x in solutions.values()]), "Challenges without solutions")
 
 # Key: The challenge as 4-tuple
 # Result: The correct operations as a list (in order)
 
-# Let's get some numbers from the user!
-while True:
+
+# Display and receive a prompt for the numbers
+def prompt_numbers():
     ch_input = input("Enter the challenge numbers: ")
-    ch_key = [int(x) for x in ch_input.split() if x.isdigit()]
-    if len(ch_key) == CHALLENGE_LENGTH:
-        for i, res in enumerate(solutions[tuple(ch_key)]):
-            print("Solution", i+1, ":", res)
-    else:
+    return [int(x) for x in ch_input.split() if x.isdigit()]
+
+
+# Let's get some numbers from the user!
+while PHASE == 1:
+    ch_key = prompt_numbers()
+
+    # If we didn't put in 4 numbers, let's let the user skip it
+    if len(ch_key) != CHALLENGE_LENGTH:
         print("Sorry, that's not a valid set of numbers...")
+        continue
+
+    # If we don't have any solutions attached, just let the user know
+    if len(solutions[tuple(ch_key)]) == 0:
+        print("> No solutions found!")
+        continue
+
+    # If there are solutions, iterate through and show them to the user
+    for i, res in enumerate(solutions[tuple(ch_key)]):
+        print("> Solution", i+1, ":", res)
+
+# We want to know how many problems can be solved with re-arrangement
+if PHASE == 2:
+    # For each item in solution, we want to permute the numbers
+    # We only store if there was no solution at first but there is now
+    # We could remove duplicates in a future version; e.g., 9 9 6 5 is a permutation of 9 9 6 5 [the 9s are swapped]
+    alts = {i: [list(j) for j in permutations(i) if len(solutions[j]) > 0] for i, v in solutions.items() if len(v) == 0}
+    print(sum([1 if len(x) == 0 else 0 for x in alts.values()]), "Challenges can be solved with re-arranging")
+
+while PHASE == 2:
+    ch_key = prompt_numbers()
+
+    # Make sure we had 4 numbers
+    if len(ch_key) != CHALLENGE_LENGTH:
+        print("Sorry, that's not a valid set of numbers...")
+        continue
+
+    # If we have a solution right now, let's just show the answers (like Phase 1)
+    if len(solutions[tuple(ch_key)]) > 0:
+        print(">> No Rearrangement Required")
+        for i, res in enumerate(solutions[tuple(ch_key)]):
+            print("> Solution", i+1, ":", res)
+        continue
+
+    # Let's iterate through possible permutations
+    if len(alts[tuple(ch_key)]) == 0:
+        print("Re-arranging would not help!")
+        continue
+
+    for alt_ch_key in alts[tuple(ch_key)]:
+        print(">> Arrangement:", " ".join([str(y) for y in alt_ch_key]))
+        for i, res in enumerate(solutions[tuple(alt_ch_key)]):
+            print("> Solution", i+1, ":", res)
+            solution_found = True
